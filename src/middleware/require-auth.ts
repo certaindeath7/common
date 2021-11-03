6344a94a716a5c2fd72d5510ee651224de85cbe4
Duc Nguyen <ducnguyentranphuc98@gmail.com>
2021-11-03 11:45:37 +1100

    change throw error for requireAuth middleware

--- src/middleware/require-auth.ts
+++ src/middleware/require-auth.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import { NotAuthorizedError } from "../errors/not-authorized-error";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
   if (!req.currentUser) {
-    throw new NotAuthorizedError();
+    return res.status(401).json({ errors: [{ msg: "User is not authorized" }] });
   }
   next();
 };
