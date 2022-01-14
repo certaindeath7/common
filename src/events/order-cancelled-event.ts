d74e1ce9aca57f817c1c04676a1fea4e4060b5f1
Duc Nguyen <ducnguyentranphuc98@gmail.com>
2022-01-14 21:25:35 +1100

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -1,12 +1,10 @@
 import { Subjects } from './subjects';
-import { OrderStatus } from '..';
 export interface IOrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
     tickets: {
       id: string;
-      price: number;
     };
   };
 }
